f2317fc3624797f2876774ba5c1e2f625bf104b8
Brian <bgswilde@gmail.com>
2021-09-02 21:51:04 -0500

    working html routes and api get route for notes

--- server.js
+++ server.js
@@ -1,31 +1,55 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./db/db.json');
-const { createNote, deleteNote } = require('./lib/notes');
+const db = require('./db/db.json');
+// const { createNote, deleteNote } = require('./lib/notes');
 
 // assigns the appropriate port or uses 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+// begin HTML routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.post('/api/notes', (req, res) => {
-    // function to create note "createdNote"
-    res.json(createdNote);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+// end HTML routes
+
+// begin API routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, notes) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(JSON.parse(notes));
+    });
+});
+
+// app.post('/api/notes', (req, res) => {
+//     // set up info on note to post
+// });
 
 app.delete('/api/notes/:id', (req, res) => {
-    // function to delete note, needing id of note from length of array
+    // removing note in db.json file
+    res.json(db)
+});
+// end API routes
+
+// wildcard, route other html requests back to the homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// listen for the active port number and log it to the back end console
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
